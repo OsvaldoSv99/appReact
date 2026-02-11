7ac664072180cb9f912e3f211be65c36305928be
Osvaldo <osvaldosv.gobti@gmail.com>
2026-02-10 21:35:00 -0600

    list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import Card from "./componentes/Card";
+import Card, { CardBody } from "./componentes/Card";
+import List from "./componentes/List";
 
 function App() {
-  return <Card />;
+  const list = ["goku", "vegeta", "tio", "asa"];
+  return (
+    <Card>
+      klajjsldkjalskjd
+      <CardBody title={"Titulo"} text={"Texto"} />
+      <List data={list} />
+    </Card>
+  );
+  // return <CardBody title={"Titulo"} text={"texto"} />;
 }
 
 export default App;
--- src/componentes/Card.tsx
+++ src/componentes/Card.tsx
@@ -1,21 +1,38 @@
-import "bootstrap";
-function Card() {
+import type { ReactNode } from "react";
+
+interface Props {
+  // children: string; // Si se pasa un tipo de dato en especifico
+  children: ReactNode; // Si se quiere pasar un componente completo
+}
+
+function Card(props: Props) {
+  const { children } = props;
   const width = {
     width: "350px",
   };
   return (
     <div className="card" style={width}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somasassaewhere
-        </a>
-      </div>
+      <div className="card-body">{children}</div>
+      {/* <div className="card-body"><CardBody {title='Hola', text = "aknsdka"}/></div> */}
     </div>
+  );
+}
+
+// Se pueden poner mas componentes en un archivo
+// Se pone export para exportar mas de un componente de un archivo
+// Para esta funcion se tiene que envolver en otra etiqueta (div) o ponerle un fragment con etiqueta <Fragment> e importando la libreria o solamente <>
+interface CardBodyProps {
+  title: string;
+  text?: string; // Con el simbolo ? se indica que el valor es opcional
+}
+
+export function CardBody(props: CardBodyProps) {
+  const { title, text } = props;
+  return (
+    <>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
+    </>
   );
 }
 
