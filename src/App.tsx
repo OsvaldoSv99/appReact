493ecaa4937f742f3e9eee28b89c55244c2e2dd7
Osvaldo <osvaldosv.gobti@gmail.com>
2026-02-16 18:42:47 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import List from "./componentes/List";
 
 function App() {
   const list = ["goku", "vegeta", "tio", "asa"];
+  const handleSelect = (elemento: string) => {
+    console.log("Imprimiento", elemento);
+  };
   return (
     <Card>
       klajjsldkjalskjd
       <CardBody title={"Titulo"} text={"Texto"} />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
     </Card>
   );
   // return <CardBody title={"Titulo"} text={"texto"} />;
